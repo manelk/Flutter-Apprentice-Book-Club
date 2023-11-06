13d4826b0fba716dded63baa67a43fca95040e45
manel kacem <manelkacem11@gmail.com>
2023-11-06 02:38:31 +0100

    commit

--- lib/apod_theme.dart
+++ lib/apod_theme.dart
@@ -4,25 +4,25 @@ import 'package:google_fonts/google_fonts.dart';
 class ApodTheme {
   // 1
   static TextTheme lightTextTheme = TextTheme(
-    bodyText1: GoogleFonts.openSans(
+    bodyLarge: GoogleFonts.openSans(
         fontSize: 14.0, fontWeight: FontWeight.w700, color: Colors.black),
-    headline1: GoogleFonts.openSans(
+    displayLarge: GoogleFonts.openSans(
         fontSize: 32.0, fontWeight: FontWeight.bold, color: Colors.black),
-    headline2: GoogleFonts.openSans(
+    displayMedium: GoogleFonts.openSans(
         fontSize: 21.0, fontWeight: FontWeight.w700, color: Colors.black),
-    headline3: GoogleFonts.openSans(
+    displaySmall: GoogleFonts.openSans(
         fontSize: 16.0, fontWeight: FontWeight.w600, color: Colors.black),
   );
 
   // 2
   static TextTheme darkTextTheme = TextTheme(
-    bodyText1: GoogleFonts.openSans(
+    bodyLarge: GoogleFonts.openSans(
         fontSize: 14.0, fontWeight: FontWeight.w600, color: Colors.white),
-    headline1: GoogleFonts.openSans(
+    displayLarge: GoogleFonts.openSans(
         fontSize: 32.0, fontWeight: FontWeight.bold, color: Colors.white),
-    headline2: GoogleFonts.openSans(
+    displayMedium: GoogleFonts.openSans(
         fontSize: 21.0, fontWeight: FontWeight.w700, color: Colors.white),
-    headline3: GoogleFonts.openSans(
+    displaySmall: GoogleFonts.openSans(
         fontSize: 16.0, fontWeight: FontWeight.w600, color: Colors.white),
   );
 
